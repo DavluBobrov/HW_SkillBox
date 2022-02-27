7ee6c97ce6a6e5495b4dbe60574e52845c5c7d99
Георгий Орлов <orlov1994@bk.ru>
2022-02-28 00:39:55 +0300

    Блок 3 задание 2

---
+++ Block_3/Block_3_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace Block_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрос пользователю
+            while (true)
+            {
+                WriteLine("Введите число");
+                string data = ReadLine();                
+                bool is_number = Int32.TryParse(data, out int number);          // Флаг проверки на введенные данные
+                if (is_number)                                                  // Проверка на то, что введено именно число
+                {
+                    // Введено число, выводим что требуется
+                    if (number % 2 == 0) WriteLine("Введённое число является Чётным.");
+                    else WriteLine("Введённое число является Нечётным.");
+                    break;                                                      // Принудительный выход из цикла
+                }
+                else
+                {
+                    // Ошибка ввода, давай по новой
+                    Clear();
+                    WriteLine("Данные введены неверно!");
+                }
+            }
+        }
+    }
+}
