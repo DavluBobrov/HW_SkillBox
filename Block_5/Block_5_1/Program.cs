e7ba8b45eebfd703fb08cd9df0517757280d8c26
Георгий Орлов <orlov1994@bk.ru>
2022-03-03 11:03:06 +0300

    Block_5

---
+++ Block_5/Block_5_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+namespace Block_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Введите любой текст через пробел:");
+            //PrintArrayWords(SplitTextInArray(ReadLine()));
+            string data = ReadLine();
+            string[] split_data = SplitTextInArray(data);
+            PrintArrayWords(split_data);
+            ReadKey();
+        } 
+
+        /// <summary>
+        /// Метод разбиения строки тества в массив
+        /// </summary>
+        /// <param name="text_to_split">Строка для ввода</param>
+        /// <returns></returns>
+        static string[] SplitTextInArray(string text_to_split)
+        {
+            return text_to_split.Split(' ');
+        }
+
+        /// <summary>
+        /// Метод печати массива слов на каждой строке
+        /// </summary>
+        /// <param name="arr">Входной массив слов</param>
+        static void PrintArrayWords(string[] arr)
+        {
+            foreach (var arg in arr)
+            {
+                WriteLine(arg);
+            }
+        }
+    }
+}
---
+++ Block_5/Block_5_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using static System.Console;
+
+namespace Block_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Введите любой текст через пробел:"); 
+            string input_text = ReadLine();
+            InverseWords(input_text);
+            ReadKey();
+        }
+
+        /// <summary>
+        /// Метод разбиения строки тества в массив
+        /// </summary>
+        /// <param name="text_to_split">Строка для ввода</param>
+        /// <returns>Возвращает массив слов</returns>
+        static string[] SplitTextInArray(string text_to_split)
+        {
+            return text_to_split.Split(' ');
+        }
+
+        /// <summary>
+        /// Метод перестановки слов наоборот
+        /// </summary>
+        ///
+        static void InverseWords(string text_fo_inverse)
+        {
+            string[] arr = SplitTextInArray(text_fo_inverse);
+            Array.Reverse(arr);
+            PrintArrayWords(arr);
+        }
+
+        /// <summary>
+        /// Метод печати массива слов на каждой строке
+        /// </summary>
+        /// <param name="arr">Входной массив слов</param>
+        static void PrintArrayWords(string[] arr)
+        {
+            foreach (var arg in arr)
+            {
+                Write($"{arg} ");
+            }
+        }
+    }
+}
