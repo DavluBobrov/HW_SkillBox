261f6dfb983802dbda267065a2b7d3ce6727bbbe
Георгий Орлов <orlov1994@bk.ru>
2022-07-18 15:19:19 +0300

    Добавил структуру для команд

---
+++ Block_12/Module_12/Module_12/Infrastucture/Commands/Base/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Module_12.Infrastucture.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged     // Событие генерируется, когда CanExecute начинает возвращать другой тип(значение), когда команда переходит из одного состояния в другое
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);          // Если false, то команду выполнить нельзя и привязанный элемент отключается автоматически
+
+        public abstract void Execute(object parameter);             // Основная логика команды, которую она должна выполнять
+    }
+}
---
+++ Block_12/Module_12/Module_12/Infrastucture/Commands/LambdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using Module_12.Infrastucture.Commands.Base;
+using System;
+
+namespace Module_12.Infrastucture.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));                     // Ругаемся, если не передали ссылку на делегат
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
