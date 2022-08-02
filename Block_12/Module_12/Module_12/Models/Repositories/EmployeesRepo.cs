61752b56730d4f93f0a47a38057cf743d4fd07d0
Георгий Орлов <orlov1994@bk.ru>
2022-08-03 00:01:15 +0300

    Добавил Репозиторий для Сотрудников

---
+++ Block_12/Module_12/Module_12/Models/Repositories/EmployeesRepo.cs
@@ -0,0 +1,29 @@
+﻿using Module_12.Models.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_12.Models.Repositories
+{
+    internal class EmployeesRepo : IEmployeesRepo
+    {
+        public EmployeesRepo()
+        {
+            InitEmployees();
+        }
+
+        private void InitEmployees()
+        {
+            List<IEmployee> employees = new();
+            employees.Add(new Manager());
+            employees.Add(new Consultant());
+            Employees = employees;
+        }
+
+        private List<IEmployee> Employees { get; set; }
+
+        public IEnumerable<IEmployee> GetEmployees() => Employees;
+    }
+}
---
+++ Block_12/Module_12/Module_12/Models/Repositories/IEmployeesRepo.cs
@@ -0,0 +1,10 @@
+﻿using Module_12.Models.Employees;
+using System.Collections.Generic;
+
+namespace Module_12.Models.Repositories
+{
+    internal interface IEmployeesRepo
+    {
+        public IEnumerable<IEmployee> GetEmployees();
+    }
+}
