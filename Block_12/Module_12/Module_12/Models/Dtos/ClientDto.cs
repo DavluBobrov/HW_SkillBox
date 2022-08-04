e8e79ec5b4f84c80ec531ab6399ac54dcd43fbc6
Георгий Орлов <orlov1994@bk.ru>
2022-08-04 22:51:22 +0300

    Добавил ClientDto

---
+++ Block_12/Module_12/Module_12/Models/Dtos/ClientDto.cs
@@ -0,0 +1,26 @@
+﻿using Module_12.Models.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Module_12.Models.EnumTypes;
+
+namespace Module_12.Models.Dtos
+{
+    internal class ClientDto
+    {
+        private string _phoneNumber;
+
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public string PhoneNumber { get => _phoneNumber; set => _phoneNumber = EditTelephone(value); }
+        public Passport PassportData { get; set; }
+        public Guid ID { get; set; }
+        public string Departament { get; init; }
+        public Dictionary<DataTypeClient, Log> EditsDataLog { get; set; } = new();
+
+        private string EditTelephone(string newPhone) => newPhone.Length == 10 ? $"{newPhone}" : this.PhoneNumber;
+    }
+}
