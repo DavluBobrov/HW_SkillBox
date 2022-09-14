8d76fc18cddf1d3a639dd0d29f96329c3ee9b0b6
Георгий Орлов <orlov1994@bk.ru>
2022-09-14 16:56:07 +0300

    Балуемся с формой, добавляем кнопки

--- Block_13/Module_13/Module_13/MainWindow.xaml.cs
+++ Block_13/Module_13/Module_13/MainWindow.xaml.cs
@@ -20,9 +20,83 @@ namespace Module_13
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Bank A = new Bank();
+        public Client SelectedClient { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Grid.ItemsSource = A.Clients;
+        }
+
+        private void Grid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            FirstNameTextBox.Text = (Grid.SelectedItem as Client).FirstName;
+            LastNameTextBox.Text = (Grid.SelectedItem as Client).LastName;
+            CheckedAccounts();
+            DepAccTextBox.Text = (Grid.SelectedItem as Client).Deposit.Bablo.Money.ToString();
+            NotDepAccTextBox.Text = (Grid.SelectedItem as Client).NotDeposit.Bablo.Money.ToString();
+            RefreshButtons();
+        }
+
+        private void CheckedAccounts()
+        {
+            if ((Grid.SelectedItem as Client).Deposit.isOpen)
+            {
+                DepAccTextBox.Visibility = Visibility.Visible;
+                DepAccTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                DepAccTextBox.Visibility = Visibility.Collapsed;
+                DepAccTextBlock.Visibility = Visibility.Collapsed;
+            }
+            if ((Grid.SelectedItem as Client).NotDeposit.isOpen)
+            {
+                NotDepAccTextBox.Visibility = Visibility.Visible;
+                NotDepAccTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                NotDepAccTextBox.Visibility = Visibility.Collapsed;
+                NotDepAccTextBlock.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void OpenAccCombo_Click(object sender, RoutedEventArgs e)
+        {
+            OpenAccComboButton.Visibility = Visibility.Collapsed;
+            TypeAccComboBox.Visibility = Visibility.Visible;
+        }
+
+        private void TypeAccComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            OPEN_ACC_Button.Visibility = Visibility.Visible;
+        }
+
+        private void OPEN_ACC_Click(object sender, RoutedEventArgs e)
+        {
+            switch (TypeAccComboBox.SelectedIndex)
+            {
+                case 0:
+                    (Grid.SelectedItem as Client).Deposit.isOpen = true;
+                    break;
+
+                case 1:
+                    (Grid.SelectedItem as Client).NotDeposit.isOpen = true;
+                    break;
+            }
+            TypeAccComboBox.SelectedIndex = -1;
+            CheckedAccounts();
+            RefreshButtons();
+        }
+
+        private void RefreshButtons()
+        {
+            OpenAccComboButton.Visibility = Visibility.Visible;
+            TypeAccComboBox.Visibility = Visibility.Collapsed;
+            OPEN_ACC_Button.Visibility = Visibility.Collapsed;
+            OPEN_ACC_Button.UpdateLayout();
         }
     }
 }
